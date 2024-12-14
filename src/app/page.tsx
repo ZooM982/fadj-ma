5feac718d3a34cb3994a53fc3330dc45ad51d549
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2024-12-14 14:16:31 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import LayoutAuth from "./authentification/layout";
 export default function Home() {
   return (
     <main className="">
-      <LayoutAuth />
+      <LayoutAuth>
+        <h1 className="hidden">Bienvenue sur la page d'accueil</h1>
+      </LayoutAuth>
     </main>
   );
 }
