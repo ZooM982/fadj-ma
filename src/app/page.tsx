f262a5fe58819a8732ab8055b698f868d31d5284
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2024-12-14 14:32:41 +0000

    test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <main className="">
       <LayoutAuth>
-        <h1 className="hidden">Bienvenue sur la page</h1>
+        <h1 className="text-center text-5xl font-semibold">
+          Bienvenue sur notre plateforme
+        </h1>
+        <p className="mt-4 text-center text-gray-600">
+          Veuillez vous connecter ou vous inscrire pour continuer.
+        </p>
       </LayoutAuth>
     </main>
   );
